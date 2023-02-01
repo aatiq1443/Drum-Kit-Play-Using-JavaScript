7e66c901000069beefd1feeae14412c4f30b5cce
Aatiq Afzal <aatiqafzal1443@gmail.com>
2023-02-01 23:09:09 +0530

    Drum Kit

---
+++ index.js
@@ -0,0 +1,96 @@
+// document.querySelector("button").addEventListener("click", handleClick);
+
+// function handleClick(){
+//     alert("I'm clikced");
+// }
+
+var size = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < size; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+        makeAnimation(buttonInnerHtml);
+
+    });
+
+}
+
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    makeAnimation(event.key);
+})
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+
+        case "s":
+
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+
+        case "d":
+
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+
+        case "j":
+
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+
+        case "k":
+
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+
+
+        case "l":
+
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+function makeAnimation(keyButton) {
+    var activeButton = document.querySelector("." + keyButton);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 220);
+}
+
+// this.innerHTML => very imp
+
+
+// function drumSound() {
+//     var audio = new Audio('sounds/tom-1.mp3');
+//     audio.play();
+
+// this.style.color = "white";
+// }
